88e0010ba566296576a32ef5f8828d29ebb984fd
Iván Sierra <sierraivan93@gmail.com>
2026-01-29 03:28:14 -0300

    feat(security): create userdetails service

---
+++ src/main/java/com/soluciones/ticketgestor/services/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.soluciones.ticketgestor.services;
+
+import com.soluciones.ticketgestor.models.User;
+import com.soluciones.ticketgestor.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("El usuario: '" + username + "' no existe"));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getEmail())
+                .password(user.getPassword())
+                .roles(user.getRole().getDescription())
+                .build();
+    }
+}
